0492b9c346f8571f35c98475d0d97ce2f63a3364
vivek1523 <vivekram4560@gmail.com>
2025-09-12 23:28:07 +0400

    Animation Implemented to portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar"
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
+    <Navbar/>
       <Hero/>
+      <ShowcaseSection/>
     </>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import AnimatedCounter from '../components/AnimatedCounter'
+import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 import { Words } from '../constants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Hero = () => {
+
+  useGSAP(()=>{
+    gsap.fromTo('.hero-text h1', {
+      y: 50,
+      opacity: 0
+    },{
+      y: 0,
+      opacity: 1,
+      stagger: 0.2,
+      duration: 2,
+      ease: "power2.inOut"
+    })
+  }) 
+
   return (
      <section id='hero' className='relative overflow-hidden'>
         <div className='absolute top-0 left-0 z-10'>
@@ -9,6 +27,7 @@ const Hero = () => {
         </div>
 
         <div className='hero-layout'>
+          {/*Left Side Layout */}
          <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
             <div className='flex flex-col gap-7'>
               <div className='hero-text'>
@@ -33,12 +52,32 @@ const Hero = () => {
                   </span>
                 </span>
                </h1>
-               <h1>into Real Projects</h1>
-               <h1>that Deliver Results</h1>
+                <h1>into Real Projects</h1>
+                <h1>that Deliver Results</h1>
               </div>
+
+               <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
+                Hi I'm Vivek, a Software developer based in Croatia with a passion for code.
+               </p>
+                
+               <Button 
+                className="md:w-80 md:h-16 w-60 h-12"
+                id="button"
+                text="See my Work"
+                />
             </div>
          </header>
+
+         {/*Right Side Layout */}
+         <figure>
+          <div className='hero-3d-layout'>
+           <HeroExperience/>
+          </div>
+         </figure>
         </div>
+  
+     <AnimatedCounter/>
+
      </section>
   )
 }
