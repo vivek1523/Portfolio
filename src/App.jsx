3ae6585130992220164f73fcbfd54cf65945d356
vivek1523 <vivekram4560@gmail.com>
2025-09-13 23:40:58 +0400

    Features Card Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import LogoSection from "./sections/LogoSection"
 import Navbar from "./components/Navbar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar/>
       <Hero/>
       <ShowcaseSection/>
+      <LogoSection/>
+      <FeatureCards/>
     </>
   )
 }
